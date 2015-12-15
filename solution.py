5e741fde27eac3626f410cc9a71062be54d9aae6
Pablo Varela <pablovarela182@gmail.com>
2015-12-15 07:49:26 +0100

    problem #2 solved

--- solution.py
+++ solution.py
@@ -1,11 +1,23 @@
 
-def solution(iterations = 10):
-    sum = 0
-    for i in range(iterations):
-        if i % 3 == 0: sum += i
-        elif i % 5 == 0: sum += i
+cache = {} # we use a dictionary to save calculated Fibonacci values (so we don't calculate them again and again)
+# cache.get(x, y) returns a value for the given key 'x'. If key is not available, then returns default value 'y'
 
+def fib(num):
+    if num < 2:
+        cache[num] = num
+    else:
+        # it gets the number from the cache if we already calculated it
+        cache[num] = cache.get(num, 0) or (fib(num-1) + fib(num-2))
+
+    return cache[num]
+
+def solution(maxfib = 10):
+    sum = 0; i = 2; current = 0
+    while current < maxfib:
+        current = fib(i)
+        if current % 2 == 0: sum += current
+        i += 1
     return sum
 
 if __name__ == "__main__":
-    print "The final sum is", solution(1000)
+    print "The final sum is", solution(4000000)
