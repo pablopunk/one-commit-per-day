b2850b20adbf8e72413af0e5c5d5839f89edd4c7
Pablo Varela <pablovarela182@gmail.com>
2015-12-16 07:46:09 +0100

    problem #3 solved

--- solution.py
+++ solution.py
@@ -1,23 +1,11 @@
 
-cache = {} # we use a dictionary to save calculated Fibonacci values (so we don't calculate them again and again)
-# cache.get(x, y) returns a value for the given key 'x'. If key is not available, then returns default value 'y'
-
-def fib(num):
-    if num < 2:
-        cache[num] = num
-    else:
-        # it gets the number from the cache if we already calculated it (get() doesn't return 0)
-        cache[num] = cache.get(num, 0) or (fib(num-1) + fib(num-2))
-
-    return cache[num]
-
-def solution(maxfib = 10):
-    sum = 0; i = 2; current = 0
-    while current < maxfib:
-        current = fib(i)
-        if current % 2 == 0: sum += current
+def solution(num = 13195):
+    i = 2
+    while i * i < num:
+        while num % i == 0:
+            num /= i # we factor 'num' as long as it keeps being divided by 'i'
         i += 1
-    return sum
+    return num # now it's gonna be the largest prime we reached (it couldn't be divided any more)
 
 if __name__ == "__main__":
-    print "The final sum is", solution(4000000)
+    print "The largest prime factor is", solution(600851475143)
